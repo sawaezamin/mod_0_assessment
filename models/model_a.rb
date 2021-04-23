7bc071b1f40d2da42097bc9f152556fd2f44a06e
Sawaez Amin <sawaezamin@gmail.com>
2021-04-22 19:28:56 -0600

    Make changes to model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+my_school = {
+  "school_name" => "Turing",
+  "school_program" => "Back end development",
+  "cohort_number" => "2105",
+  "number_of_students" => "32",
+  "Good_program" => "true",
+  "easy_program" => "false",
+  "my_info" => "['Sawaez Amin', 'age: 29', 'male']",
+  "different_cohorts" => "['FE:2105', 'FE:2107', 'BE:2107']"
+
+}
