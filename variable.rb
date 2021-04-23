a59e91dd2e64573d9660f0b426c606db096f9bec
Sawaez Amin <sawaezamin@gmail.com>
2021-04-22 19:13:00 -0600

    Complete program specific challenge

---
+++ variable.rb
@@ -0,0 +1,11 @@
+famous_person = "Nikola Tesla"
+is_active = false
+famous_person = "Cristiano Ronaldo"
+num_3 = 3
+num_9 = 9
+sum = num_3 + num_9
+
+animals = ["tiger", "eagle", "lion"]
+animals.shift
+p animals
+famous_person.length
