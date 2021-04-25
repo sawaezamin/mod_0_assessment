621a8e68153df24b38de75880ae405386826af18
Sawaez Amin <sawaezamin@gmail.com>
2021-04-25 14:06:32 -0600

    Make Mod_0 changes

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,11 +1,11 @@
 my_school = {
-  "school_name" => "Turing",
-  "school_program" => "Back end development",
-  "cohort_number" => "2105",
-  "number_of_students" => "32",
-  "Good_program" => "true",
-  "easy_program" => "false",
-  "my_info" => "['Sawaez Amin', 'age: 29', 'male']",
-  "different_cohorts" => "['FE:2105', 'FE:2107', 'BE:2107']"
+  school_name: "Turing",
+  school_program: "Back end development",
+  cohort_number: 2105,
+  number_of_students: 32,
+  Good_program: true,
+  easy_program: false,
+  my_info: ["Sawaez Amin", "Back end", "2105"],
+  different_cohorts: ["FE 2105", "FE 2107", "BE 2107"]
 
 }
--- models/model_b.rb
+++ models/model_b.rb
@@ -1,10 +1,10 @@
 my_dog = {
-  "dog_name" => "Bernie",
-  "dog_breed" => "berenese mountain dog",
-  "dog_age_in_human_years" => "3",
-  "dog_age_in_dog_years" => "31.2",
-  "best_dog" => "true",
-  "smart_dog" => "true",
-  "favorite_toys" => "['ball', 'bone', 'newspaper']",
-  "favorite_hobbies" => "['walking', 'running', 'swimming']"
+  dog_name: 'Bernie',
+  dog_breed: 'berenese mountain dog',
+  dog_age_in_human_years: 3,
+  dog_age_in_dog_years: 31.2,
+  best_dog: true,
+  smart_dog: true,
+  favorite_toys: ['ball', 'bone', 'newspaper'],
+  favorite_hobbies: ['walking', 'running', 'swimming']
 }
